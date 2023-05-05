9017be120daf5d83682d5d8b2d954d1230f9d57c
Jeziel Almeida <jeziel.almeida16@gmail.com>
2023-05-05 19:42:40 -0300

    Create the verification of the text

---
+++ scripts.js
@@ -0,0 +1,50 @@
+const text = document.querySelector("#text")
+const input = document.querySelector("#input")
+const restart = document.querySelector("#restart")
+const result = document.querySelector("#result")
+const history = document.querySelector("#history")
+const changeThemeBtn = document.querySelector("#changeTheme")
+
+const texts = [
+    "When in doubt, go to the library",
+    "I solemnly swear that I am up to no good",
+    "Hogwarts will always be there to welcome you home",
+    "Don't let the muggles get you down",
+    "It does not do to dwell on dreams and forget to live",
+]
+
+function newText() {
+    const index = Math.floor(Math.random() * texts.length)
+    text.textContent = texts[index]
+}
+
+function updateTest() {
+    start()
+
+    if (input.value === text.textContent) {
+        verify()
+    }
+}
+
+function start() {
+
+    const testStatus = JSON.parse(localStorage.getItem("testInCourse")) //transforma uma string em um booleano
+
+    if (!testStatus) {
+        localStorage.setItem("startTime", new Date().getTime())
+        localStorage.setItem("testInCourse", true)
+    }
+}
+
+function verify() {
+    const finalTime = new Date().getTime()
+    const startTime = parseInt(localStorage.getItem("startTime"))
+    const timeSpent = (finalTime - startTime) / 1000
+
+    localStorage.setItem("testInCourse", false)
+    input.value = ""
+}
+
+input.addEventListener("keyup", updateTest)
+
+newText()
